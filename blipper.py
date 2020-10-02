fa21b2ab8654c91dc4a4f71bd419cc2da450999b
Fredrik Sloberg <sloberg@gmail.com>
2020-10-03 01:41:29 +0200

    first commit

---
+++ blipper.py
@@ -0,0 +1,96 @@
+import sqlite3
+import json
+
+conn = sqlite3.connect("lager.db")
+
+c = conn.cursor()
+
+#skapar table
+def create_table():
+    c.execute("""CREATE TABLE lager (artikelnummer,artikelnamn,antal)""")
+
+    conn.commit()
+
+
+#läs in nya artiklar
+def insert_data(x,y,z):
+    c.execute("INSERT INTO lager VALUES (?,?,?)", (x,y,z))
+
+    conn.commit()
+
+
+#Kollar upp artikelnamnet
+def check_artname(artnr):
+    with open('lager.json') as f:
+        data = json.load(f)
+
+    temp =[]
+
+    for item in data['artikel']:        
+        if str(artnr) in item.get('artnr'):
+            art_namn = item['artnamn']
+            return art_namn
+
+
+#visar allt
+def sel_all():
+    items = c.execute("SELECT rowid, * FROM lager")
+    for item in items:
+        print(item)
+
+
+#updaterar antal
+def update_antal(artnr):
+    c.execute("SELECT rowid, * FROM lager WHERE artikelnummer = (?)",[artnr,] )
+
+    records = c.fetchall()
+
+    for row in records:
+        temp = int(row[3])
+        temp = temp + 1
+
+    
+
+    c.execute("UPDATE lager SET antal = ? WHERE artikelnummer = (?)", [temp,artnr,]) 
+    conn.commit() 
+
+def scanner():
+
+    inp = input()
+    inp = inp.upper()
+    return inp
+
+def check_db(artnr):
+    c.execute("SELECT rowid, * FROM lager WHERE artikelnummer = (?)",[artnr,] )
+
+    records = c.fetchall()
+
+    for row in records:
+        temp = row[1]
+        return temp
+
+
+while True:
+    z = 1
+
+
+
+    scan_art = scanner()
+
+    ret_artnamn = check_artname(scan_art)
+
+    ret_artnum = check_db(scan_art)
+
+
+    if scan_art == ret_artnum:
+        print('update')
+        update_antal(scan_art)
+    
+    else:
+        print('insert')
+        insert_data(scan_art,ret_artnamn,z)
+    
+
+    sel_all()
+    
+conn.close()
---
+++ manage.py
@@ -0,0 +1,62 @@
+import sqlite3
+import json
+
+conn = sqlite3.connect("lager.db")
+
+c = conn.cursor()
+
+
+def del_all():
+    c.execute("SELECT rowid, * FROM lager")
+    items = c.fetchall()
+
+    for item in items:
+        temp = item[0]
+        c.execute("DELETE from lager WHERE rowid = (?)", [temp,])
+        conn.commit()
+
+def del_one():
+    c.execute("SELECT rowid, * FROM lager")
+    items = c.fetchall()
+
+    temp = input('number to delete? ')
+
+    c.execute("DELETE from lager WHERE rowid = (?)", [temp,])
+    conn.commit()
+
+def exp_json():
+    c.execute("SELECT * FROM lager")
+
+    items = c.fetchall()
+    
+    art=[]
+    for item in items:
+        temp = {"artnr": item[0], "languages": item[1], "antal":item[2]}
+        art.append(temp)
+    print(art)
+
+    with open("databas.json", "w",) as json_file:
+        json.dump(art, json_file, indent=4)
+
+    print('exported to json')
+
+def see_all():
+    c.execute("SELECT rowid, * FROM lager")
+    items = c.fetchall()
+    for item in items:
+        print(item)
+
+
+"""
+while True:
+    see_all()
+    del_one()
+"""
+
+exp_json()
+
+#del_all()
+
+
+
+conn.close()
